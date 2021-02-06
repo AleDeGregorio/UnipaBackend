ebc879999b9fed8dde311cf9eec6547581f6344e
AleDeGregorio <alessio.degregorio.98@gmail.com>
2021-02-06 15:08:07 +0100

    Credenziali db hostato

--- db/config.js
+++ db/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     mysql: {
-        host: 'localhost',
+        host: 'sql7.freemysqlhosting.net',
         port: 3306,
-        user: 'user',
-        database: 'progetto',
-        password: 'user',
+        user: 'sql7391138',
+        database: 'sql7391138',
+        password: 'YKhHkDPWTy',
         multipleStatements: true
     }
 }
